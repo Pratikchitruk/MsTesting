11d96b65b90b4228a78267a5b37b8602f324879f
PratikChitruk <pratik.chitruk@gmail.com>
2022-04-15 20:50:29 +0530

    [pratik] Program.cs

---
+++ Regex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+
+            EnterFirstName.FirstName();
+            EnterLastName.LastName();
+            MobileNo.MyNo();
+            Email.MyEmail();
+            Password.MyPassword();
+            Addspacial.AddspacialPassword();
+            AddUpeer.AddUpperPassword();
+         
+
+
+                
+        }
+
+    }
+}
